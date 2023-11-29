2c3effc71732c88724c9ff4e0992fb15083edc37
Giray Hakan <girayhakan23@gmail.com>
2023-11-29 23:15:16 +0300

    Model version updated

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ if audio:
                 audio_bytes_io = BytesIO(audio['bytes'])
 
                 output = replicate.run(
-                    "lucataco/xtts-v2:684bc3855b37866c0c65add2ff39c78f3dea3f4ff103a436465326e0f438d55",
+                    "lucataco/xtts-v2:684bc3855b37866c0c65add2ff39c78f3dea3f4ff103a436465326e0f438d55e",
                     input={
                              "text": user_text,
                              "speaker": audio_bytes_io,
